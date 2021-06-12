86f8893994c81e0379c64a70cfddd6d499eaaf77
Sean Dwyer <spdwyer06@gmail.com>
2021-06-12 13:10:57 -0400

    Initial commit

---
+++ GeneralStore_Web/Controllers/ProductController.cs
@@ -0,0 +1,109 @@
+﻿using GeneralStore_Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore_Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            var products = _db.Products.ToList();
+            var orderedList = products.OrderBy(product => product.Name).ToList();
+
+            return View(orderedList);
+            //return View(_db.Products.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+                return View(product);
+
+            _db.Products.Add(product);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int? productId)
+        {
+            if (productId == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var product = _db.Products.Find(productId);
+
+            if (product == null)
+                return HttpNotFound();
+
+            return View(product);
+        }
+
+        public ActionResult Edit(int? productId)
+        {
+            if (productId == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var product = _db.Products.Find(productId);
+
+            if (product == null)
+                return HttpNotFound();
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if (!ModelState.IsValid)
+                return View(product);
+
+            _db.Entry(product).State = EntityState.Modified;
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int? productId)
+        {
+            if (productId == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var product = _db.Products.Find(productId);
+
+            if (product == null)
+                return HttpNotFound();
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int productId)
+        {
+            var product = _db.Products.Find(productId);
+
+            _db.Products.Remove(product);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ GeneralStore_Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GeneralStore_Web.Startup))]
+namespace GeneralStore_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
